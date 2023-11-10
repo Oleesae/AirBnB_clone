017210d22918586373cfea24adb0ccbb26f2d649
Eboka Olisaemeka <ebokaolisaemeka@gmail.com>
2023-11-10 16:05:56 +0100

    show and destroy to console

--- console.py
+++ console.py
@@ -28,24 +28,73 @@ class HBNBCommand(cmd.Cmd):
 
         return self.do_EOF(line)
 
-    def do_create(self, arg):
+    def do_create(self, args):
         """USAGE: create <class>
         Creates a new class and prints its id.
         """
-        if not arg:
+        if not args:
             print("** class name missing **")
-        elif arg != "BaseModel":
+        elif args != "BaseModel":
             print("** class doesn't exist **")
         else:
-            print(eval(arg)().id)
+            print(eval(args)().id)
             storage.save()
 
-    def do_show(self, arg):
+    def do_show(self, args):
         """USAGE: show <class> <id>
         Prints the string representation of an instance
         based on the class name and id.
         """
 
+        new = args.split(" ")
+        cl_name = new[0]
+        cl_id = new[1]
+
+        if not cl_name:
+            print("** class name missing **")
+            return
+
+        if cl_name != "BaseModel":
+            print("** class doesn't exist **")
+            return
+
+        if not cl_id:
+            print("** instance id missing **")
+            return
+
+        key = cl_name + "." + cl_id
+        try:
+            print(storage.all()[key])
+        except KeyError:
+            print("** no instance found **")
+
+    def do_destroy(self, args):
+        """USAGE: destroy classname id
+        deletes an instance based on the class name and id.
+        """
+
+        new = args.split(" ")
+        cl_name = new[0]
+        cl_id = new[1]
+
+        if not cl_name:
+            print("** class name missing **")
+            return
+
+        if cl_name != "BaseModel":
+            print("** class doesn't exist **")
+            return
+
+        if not cl_id:
+            print("** instance id missing **")
+            return
+
+        key = cl_name + "." + cl_id
+        try:
+            del storage.all()[key]
+            storage.save()
+        except KeyError:
+            print("** no instance found **")
         
 
 
